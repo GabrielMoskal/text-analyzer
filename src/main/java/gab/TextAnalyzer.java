c0538b3686d27adb4062d63e12c4c84aef563e16
Gabriel <gab.mos92@gmail.com>
2022-12-17 09:34:45 +0100

    Initial commit

---
+++ src/main/java/gab/TextAnalyzer.java
@@ -0,0 +1,13 @@
+package gab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextAnalyzer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextAnalyzer.class, args);
+    }
+
+}
---
+++ src/test/java/gab/TextAnalyzerTests.java
@@ -0,0 +1,13 @@
+package gab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextAnalyzerTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
